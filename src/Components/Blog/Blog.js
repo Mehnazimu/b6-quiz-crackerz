8c9c131d56c4fd8363d44ab802e7244f978cb65c
mehnazimu <mehnaz.imu@gmail.com>
2022-10-12 21:53:09 +0600

    Blog section added

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import './Blog.css';
 
 const Blog = () => {
-    return (
-        <div>
-          <h2>this is blog</h2>  
+  return (
+    <div className='b-div'>
+      <h2>Blog</h2>
+      <div className='blog'>
+        <div >
+          <h2>What is the purpose of react router?</h2>
+          <p>
+            React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL. <br /> ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route.</p>
         </div>
-    );
+        <div>
+          <h2>How does context api work?</h2>
+          <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux. <br />
+            Context is a built-in API introduced in ​​React 16.3. It makes it possible to pass data from parent to children nested deep down the component tree directly, instead of passing it down through a chain of props.
+          </p>
+
+        </div>
+        <div>
+          <h2>What is useRef hook in React?</h2>
+          <p>The useRef Hook allows you to persist values between renders. It can be used to store a mutable value that does not cause a re-render when updated. It can be used to access a DOM element directly. <br />
+            useRef returns a mutable ref object whose .current property is initialized to the passed argument ( initialValue ). The returned object will persist for the full lifetime of the component. Essentially, useRef is like a “box” that can hold a mutable value in its .current property.</p>
+
+        </div>
+
+      </div>
+    </div>
+  );
 };
 
-export default Blog;<h2>this is blog</h2>
+export default Blog; <h2>this is blog</h2>
